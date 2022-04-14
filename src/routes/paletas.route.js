a8321f8ebdf97f19fac4887569f6f813a94a3146
Monica Sumie Hieda Dias <msumiedias@gmail.com>
2022-04-14 16:57:19 -0300

    post put delete

--- src/routes/paletas.route.js
+++ src/routes/paletas.route.js
@@ -3,5 +3,8 @@ const controllerPaletas = require('../controllers/paletas.controller');
 
 route.get('/todas-paletas', controllerPaletas.findAllPaletasController);
 route.get('/paleta/:id', controllerPaletas.findByIdPaletaController);
+route.post('/create', controllerPaletas.createPaletaController);
+route.put('/update/:id', controllerPaletas.updatePaletaController);
+route.delete('/delete/:id', controllerPaletas.deletePaletaController);
 
 module.exports = route;
--- src/services/paletas.service.js
+++ src/services/paletas.service.js
@@ -142,7 +142,29 @@ const findByIdPaletaService = (parametroId) => {
   return paletas.find((paleta) => paleta.id === parametroId);
 };
 
+const createPaletaService = (newPaleta) => {
+  const newId = paletas.length + 1;
+  newPaleta.id = newId;
+  paletas.push(newPaleta);
+  return newPaleta;
+};
+
+const updatePaletaService = (id, paletaEdited) => {
+  paletaEdited['id'] = id;
+  const paletaIndex = paletas.findIndex((paleta) => paleta.id == id);
+  paletas[paletaIndex] = paletaEdited;
+  return paletaEdited;
+};
+
+const deletePaletaService = (id) => {
+  const paletaIndex = paletas.findIndex((paleta) => paleta.id == id);
+  return paletas.splice(paletaIndex, 1);
+};
+
 module.exports = {
   findAllPaletasService,
   findByIdPaletaService,
+  createPaletaService,
+  updatePaletaService,
+  deletePaletaService,
 };
