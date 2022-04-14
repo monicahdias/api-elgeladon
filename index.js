7943d79572098c1655518f3076e1018f93c2ec52
Monica Sumie Hieda Dias <msumiedias@gmail.com>
2022-04-14 16:02:08 -0300

    mvc

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const cors = require('cors');
+const route = require('./src/routes/paletas.route');
+
 const port = 3000;
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use('/paletas', route);
 
 //Lista de Paletas
 
