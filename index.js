66cd8ea4ea3847da0158cb1a54d5f87317a34d2a
Monica Sumie Hieda Dias <msumiedias@gmail.com>
2022-04-16 19:14:16 -0300

    index correct

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const cors = require('cors');
-const route = require('./src/routes/paletas.route');
+const routes = require('./src/routes/paletas.route');
 
 const port = 3000;
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use('/paletas', route);
+app.use('/paletas', routes);
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
